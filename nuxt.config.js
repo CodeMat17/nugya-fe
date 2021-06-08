3a042089951d0228fe639fc10d9c0c9b073de80b
Chukwu Matthew <matthewnfvcb@gmail.com>
2021-06-08 13:42:50 +0100

    updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,10 @@ export default {
             theme_color: '#000',
             background_color: '#000',
         },
+        icon: {
+            source: '/static/icon.png',
+            fileName: 'icon.png',
+        },
         workbox: {
             runtimeCaching: [{
                     urlPattern: 'https://fonts.googleapis.com/.*',
